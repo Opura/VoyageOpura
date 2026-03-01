8ede8639482af5d195ee6a93caa629e060656365
Opura <apeangopura@gmail.com>
2026-03-01 10:55:09 +0100

    Hero 1.0

--- src/app/features/homepage/homepage.ts
+++ src/app/features/homepage/homepage.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 import { Header } from "../../shared/header/header";
+import { Hero } from "./hero/hero";
+import { Promoted } from "./promoted/promoted";
+import { Famous } from "./famous/famous";
+import { Search } from "./search/search";
+import { Footer } from "../../shared/footer/footer";
 
 @Component({
   selector: 'app-homepage',
-  imports: [Header],
+  imports: [Header, Hero, Promoted, Famous, Search, Footer],
   templateUrl: './homepage.html',
   styleUrl: './homepage.css',
 })
